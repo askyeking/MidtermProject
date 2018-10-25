cc94f9fb6a1f28894de97f2423307e129d1cc9a6
ndobrosa <ndobrosa@gmail.com>
2018-10-25 09:53:21 -0600

    Stub out schema in Mysql Workbench

---
+++ DB/recipemeetup.sql
@@ -0,0 +1,38 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema recipemeetupdb
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `recipemeetupdb` ;
+
+-- -----------------------------------------------------
+-- Schema recipemeetupdb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `recipemeetupdb` DEFAULT CHARACTER SET utf8 ;
+USE `recipemeetupdb` ;
+
+-- -----------------------------------------------------
+-- Table `user`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `user` ;
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` INT NOT NULL,
+  `usercol` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+SET SQL_MODE = '';
+DROP USER IF EXISTS feralDesolation@locahost;
+SET SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+CREATE USER 'feralDesolation'@'locahost' IDENTIFIED BY 'student';
+
+GRANT SELECT, INSERT, TRIGGER, UPDATE, DELETE ON TABLE * TO 'feralDesolation'@'locahost';
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
