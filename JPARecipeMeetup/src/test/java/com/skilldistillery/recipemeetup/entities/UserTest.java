d327bd6e1fea9502b46be6b66df717004eac4e99
Blake Longfield <blakelongfield@gmail.com>
2018-10-26 11:33:43 -0600

    updates to various tests

--- JPARecipeMeetup/src/test/java/com/skilldistillery/recipemeetup/entities/UserTest.java
+++ JPARecipeMeetup/src/test/java/com/skilldistillery/recipemeetup/entities/UserTest.java
@@ -1,6 +1,6 @@
 package com.skilldistillery.recipemeetup.entities;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
