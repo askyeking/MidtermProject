cdd16eda42bbdcab226c830b67a67be362e85434
askyeking <anthony.s.king1992@gmail.com>
2018-10-26 11:20:29 -0600

    wrote RecipeComment tests, stubbed out meetupCommentTest

---
+++ JPARecipeMeetup/src/test/java/com/skilldistillery/recipemeetup/entities/RecipeCommentTest.java
@@ -0,0 +1,51 @@
+package com.skilldistillery.recipemeetup.entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class RecipeCommentTest {
+
+	private static EntityManagerFactory emf;
+	EntityManager em;
+	private RecipeComment rc;
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception{
+		emf = Persistence.createEntityManagerFactory("recipemeetup");
+		
+	}
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		rc = em.find(RecipeComment.class, 1);
+	}
+	
+	@AfterAll
+	static void tearDownAfterClasS() {
+		emf.close();
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+	}
+	@Test
+	void test() {
+		assertEquals(1, rc.getUserId());
+		assertEquals(3, rc.getRecipeId());
+		assertEquals("Too much meat for my taste", rc.getComment());
+		assertEquals(true, rc.getActive());
+		assertEquals("2018-10-26",rc.getTimeStamp().toString().substring(0, 10));
+		System.out.println(rc.getTimeStamp());
+	}
+
+}
