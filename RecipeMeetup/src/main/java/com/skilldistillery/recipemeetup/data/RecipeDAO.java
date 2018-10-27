e0d93efffc6e50049e5d4cbb90274e2ae6aba67e
newel <newel_21@yahoo.com>
2018-10-27 12:25:28 -0600

    Added showRecentRecipe method

--- RecipeMeetup/src/main/java/com/skilldistillery/recipemeetup/data/RecipeDAO.java
+++ RecipeMeetup/src/main/java/com/skilldistillery/recipemeetup/data/RecipeDAO.java
@@ -9,6 +9,7 @@ public interface RecipeDAO {
 	void createRecipe(Recipe recipe);
 	Recipe showRecipe(Recipe recipe);
 	List<Recipe> showMoreRecipes();
+	List<Recipe> showRecentRecipes();
 	Recipe updateRecipe(Recipe updateRecipe);
 	boolean deleteRecipe(Recipe recipe);
 	boolean deleteRecipeById(int id);
