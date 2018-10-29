06b91d4d0029d2d6f0b3276d20bc9c7c89019093
askyeking <anthony.s.king1992@gmail.com>
2018-10-29 17:58:20 -0600

    moddified comment posting methods

--- RecipeMeetup/src/main/java/com/skilldistillery/recipemeetup/data/RecipeCommentDAO.java
+++ RecipeMeetup/src/main/java/com/skilldistillery/recipemeetup/data/RecipeCommentDAO.java
@@ -2,12 +2,13 @@ package com.skilldistillery.recipemeetup.data;
 
 import java.util.List;
 
+import com.skilldistillery.recipemeetup.entities.Recipe;
 import com.skilldistillery.recipemeetup.entities.RecipeComment;
 import com.skilldistillery.recipemeetup.entities.User;
 
 public interface RecipeCommentDAO {
 	
-	RecipeComment postRecipeComment(RecipeComment comment, User author);
+	RecipeComment postRecipeComment(Recipe recipe, RecipeComment comment, User author);
 	List<RecipeComment> showAllRecipeComments(int id);
 	RecipeComment editRecipeComment(RecipeComment updateComment, User author);
 	boolean deleteRecipeComment(RecipeComment comment, User author);
